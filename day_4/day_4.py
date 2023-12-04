8a2882e6cc9efe3fc363630e6459def291de5cfe
Milo Brown <milo.brown@tuni.fi>
2023-12-04 16:08:26 +0200

    d4p2-initial-solution

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -4,11 +4,19 @@ Code written by Milo
 Day4: Scratchcards
 """
 import re  # For splitting the input.
+from collections import defaultdict  # For counting the cards.
 
 # Deal with input.
 with open("day_4/input.txt", "r") as file:
-    data = list(map(lambda x: [column.split() for column in x],
-                [re.split(': | \| ',row)[1:] for row in file.read().splitlines()]))
+    data = list([int(id[1]),win,card] for id, win, card in
+                map(lambda x: [column.split() for column in x],
+                [re.split(': | \| ',row) for row in file.read().splitlines()]))
+
+def matches(tic):
+    """
+    Helper function to count the number of matches in a ticket.
+    """
+    return len(list(filter(lambda x: x in tic[1], tic[2])))
 
 def part1():
     """
@@ -19,23 +27,32 @@ def part1():
 
     Score: 1 for first match and doubled for each match after that.
 
-    Answer for part1:
+    Answer for part1: 26218
     """
 
     return sum(map
                (lambda x: 2**(x-1) , filter
                 (lambda z: z > 0, map
-                 (lambda x: len(list(filter
-                  (lambda y: y in x[0], x[1]))), data)))
+                 (matches, data)))
             )
 
 def part2():
     """
     PART2
+    Each winning card wins copies of the next cards,
+    according to the number of matches.
 
     Answer for part2:
     """
-    pass
+
+    cards_won = defaultdict(int)
+
+    for id,row in enumerate(data):
+        cards_won[id] += 1
+        for match_id in range(matches(row)):
+            cards_won[id+1+match_id] += cards_won[id]
+
+    return(sum(cards_won.values()))
 
 def main():
     print(f"Part1: {part1()}\nPart2: {part2()}")
