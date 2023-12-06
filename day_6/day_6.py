052c1a60904bf4b7c70d3af3f09d16674cd7c3f1
Milo Brown <milo.brown@tuni.fi>
2023-12-06 23:22:52 +0200

    solve-d6-using-quadratic-formula

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,16 +1,24 @@
+import math
+
 """
 Advent Of Code 2023
 Code written by Milo
 Day6: Wait For It
 """
-import numpy as np  # For product function.
 
 # Deal with input.
 with open("day_6/input.txt", "r") as file:
     data = [row.split()[1:] for row in file.readlines()]
 
 def count_possible_wins(time, distance):
-    return len([x for x in range(time) if (time - x) * x > distance])
+    """
+    Helper function that solves the amount of
+    possible wins using the quadratic formula.
+    """
+    root1 = (float(time) + math.sqrt(float(time)**2 - 4.0 * float(distance))) / 2.0 - 1.0
+    root2 = (float(time) - math.sqrt(float(time)**2 - 4.0 * float(distance))) / 2.0 + 1.0
+
+    return math.ceil(root1) - math.floor(root2) +1
 
 def part1():
     """
@@ -19,8 +27,9 @@ def part1():
     Answer for part1: 3317888
     """
 
-    times,distances = [list(map(int,row)) for row in [*data]]
-    return np.prod([count_possible_wins(x, y) for x, y in zip(times, distances)])
+    times, distances = [list(map(int, row)) for row in [*data]]
+    return math.prod([count_possible_wins(x, y) for x, y in zip(times, distances)])
+
 
 def part2():
     """
@@ -30,25 +39,7 @@ def part2():
     """
 
     time, distance = [int("".join(row)) for row in data]
-
-    def binary_search(is_left):
-        left = 0
-        right = time
-        while left < right:
-            mid = (left + right) // 2
-            if (time - mid) * mid > distance:
-                if is_left:
-                    right = mid
-                else:
-                    left = mid + 1
-            else:
-                if is_left:
-                    left = mid + 1
-                else:
-                    right = mid
-        return left
-
-    return binary_search(False) - binary_search(True)
+    return count_possible_wins(time, distance)
 
 def main():
     print(f"Part1: {part1()}\nPart2: {part2()}")
