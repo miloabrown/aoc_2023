ecce94ea67ac9170aa0c2e6765936185f84750ee
Milo Brown <milo.brown@tuni.fi>
2023-12-21 14:47:20 +0200

    d16-clean-up

--- day_16/day_16.py
+++ day_16/day_16.py
@@ -1,131 +1,79 @@
 """
 Advent Of Code 2023
 Code written by Milo
+Solution based partially on solution by HyperNeutrino
 Day 16: The floor will be lava
 """
 from collections import deque
 
 # Deal with input.
 with open("day_16/input.txt", "r") as file:
-    data = file.read().splitlines()
+    grid = file.read().splitlines()
 
-
-"""
-data is a 2d grid
-beam starts at 0,0 going right
-calculate how many spaces are hit by at least one beam
-Beam behavior:
-- "." continue in same direction
-- "/" or "\" turn 90 degrees
-- "|" split into two beams if hiting from left or right, otherwise continue in same direction
-- "-" split into two beams if hiting from top or bottom, otherwise continue in same direction
-
-"""
-def beam(start, direction):
+def count_hits(start):
     """
     Function to calculate how many tiles are hit by a beam
-    starting at start and going in direction
+    starting with given parameter.
+
+    start/parameter: tuple of (row, col, delta_r, delta_c)
     """
-    tiles_hit = set()
-    checked = set()
-    count = 0
-    directions = {
-        "r": (0,1),
-        "l": (0,-1),
-        "u": (-1,0),
-        "d": (1,0)
-    }
 
-    tile_queue = deque()
-    tile_queue.append((start, direction))
+    a = [start]
+    seen = set()
+    q = deque(a)
 
-    while tile_queue:
-        tile, direction = tile_queue.popleft()
-        #check inbounds
-        if not 0 <= tile[0] < len(data) or not 0 <= tile[1] < len(data[0]) or (tile,direction) in checked:
+    while q:
+        row, col, delta_r, delta_c = q.popleft()
+
+        row += delta_r
+        col += delta_c
+
+        if not 0 <= row < len(grid) or not 0 <= col < len(grid[0]):
             continue
 
-        tiles_hit.add(tile)
-        checked.add((tile, direction))
+        ch = grid[row][col]
 
-        if data[tile[0]][tile[1]] == ".":
-            tile_queue.append(((tile[0]+directions[direction][0], tile[1]+directions[direction][1]), direction))
-        elif data[tile[0]][tile[1]] == "/":
-            if direction == "r":
-                tile_queue.append(((tile[0]+directions["u"][0], tile[1]+directions["u"][1]), "u"))
-            elif direction == "l":
-                tile_queue.append(((tile[0]+directions["d"][0], tile[1]+directions["d"][1]), "d"))
-            elif direction == "u":
-                tile_queue.append(((tile[0]+directions["r"][0], tile[1]+directions["r"][1]), "r"))
-            elif direction == "d":
-                tile_queue.append(((tile[0]+directions["l"][0], tile[1]+directions["l"][1]), "l"))
+        if ch == "." or (ch == "-" and delta_c != 0) or (ch == "|" and delta_r != 0):
+            if (row, col, delta_r, delta_c) not in seen:
+                seen.add((row, col, delta_r, delta_c))
+                q.append((row, col, delta_r, delta_c))
+        elif ch == "/":
+            delta_r, delta_c = -delta_c, -delta_r
+            if (row, col, delta_r, delta_c) not in seen:
+                seen.add((row, col, delta_r, delta_c))
+                q.append((row, col, delta_r, delta_c))
 
-        elif data[tile[0]][tile[1]] == "\\":
-            if direction == "r":
-                tile_queue.append(((tile[0]+directions["d"][0], tile[1]+directions["d"][1]), "d"))
-            elif direction == "l":
-                tile_queue.append(((tile[0]+directions["u"][0], tile[1]+directions["u"][1]), "u"))
-            elif direction == "u":
-                tile_queue.append(((tile[0]+directions["l"][0], tile[1]+directions["l"][1]), "l"))
-            elif direction == "d":
-                tile_queue.append(((tile[0]+directions["r"][0], tile[1]+directions["r"][1]), "r"))
+        elif ch == "\\":
 
-        elif data[tile[0]][tile[1]] == "|":
-            if direction == "r" or direction == "l":
-                tile_queue.append(((tile[0]+directions["u"][0], tile[1]+directions["u"][1]), "u"))
-                tile_queue.append(((tile[0]+directions["d"][0], tile[1]+directions["d"][1]), "d"))
-            else:
-                tile_queue.append(((tile[0]+directions[direction][0], tile[1]+directions[direction][1]), direction))
+            delta_r, delta_c = delta_c, delta_r
+            if (row, col, delta_r, delta_c) not in seen:
+                seen.add((row, col, delta_r, delta_c))
+                q.append((row, col, delta_r, delta_c))
 
-        elif data[tile[0]][tile[1]] == "-":
-            if direction == "u" or direction == "d":
-                tile_queue.append(((tile[0]+directions["r"][0], tile[1]+directions["r"][1]), "r"))
-                tile_queue.append(((tile[0]+directions["l"][0], tile[1]+directions["l"][1]), "l"))
-            else:
-                tile_queue.append(((tile[0]+directions[direction][0], tile[1]+directions[direction][1]), direction))
+        else:
+            for delta_r ,delta_c in [(1, 0), (-1, 0)] if ch == "|" else [(0, 1), (0, -1)]:
+                if (row, col, delta_r, delta_c) not in seen:
+                    seen.add((row, col, delta_r, delta_c))
+                    q.append((row, col, delta_r, delta_c))
 
-        #need something to stop infinite loops
-        count+=1
-        if count > len(data)*len(data[0])*10:
-            break
-
-    return len(tiles_hit)
+    coords = {(r,c) for r, c, _, _ in seen}
+    return (len(coords))
 
 def part1():
-    """
-    PART1
-
-    Answer for part1: 7951
-    """
-    return beam((0,0), "r")
+    return count_hits((0, -1, 0, 1))
 
 def part2():
-    """
-    PART2
-    Where to start the beam and what direction
-    to hit the most tiles?
+    max_val = max(
+        max(count_hits((row, -1, 0, 1)), count_hits((row, len(grid[0]), 0, -1)))
+        for row in range(len(grid))
+    )
 
-    it can start from any edge, going in that direction
-    or from any corner going in one of the two directions
+    max_val = max(
+        max(count_hits((-1, col, 1, 0)), count_hits((len(grid), col, -1, 0)))
+        for col in range(len(grid[0]))
+    )
 
-    Answer for part2: 8148
-    """
-
-    #top row
-    top_row = [(0, i) for i in range(len(data[0]))]
-    #bottom row
-    bottom_row = [(len(data)-1, i) for i in range(len(data[0]))]
-    #left column
-    left_column = [(i, 0) for i in range(len(data))]
-    #right column
-    right_column = [(i, len(data[0])-1) for i in range(len(data))]
-
-    top_max = max([beam(tile, "d") for tile in top_row])
-    bottom_max = max([beam(tile, "u") for tile in bottom_row])
-    left_max = max([beam(tile, "r") for tile in left_column])
-    right_max = max([beam(tile, "l") for tile in right_column])
-
-    return max(top_max, bottom_max, left_max, right_max)
+    return max_val
 
 def main():
     print(f"Part1: {part1()}\nPart2: {part2()}")
